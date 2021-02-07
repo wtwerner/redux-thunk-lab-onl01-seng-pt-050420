02c57c7e1612e71e94e1019b1a513fb3b399e5eb
wtwerner <wtwerner@gmail.com>
2021-02-07 11:17:27 -0600

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,12 @@
-// write your CatList component here
+import { render } from 'enzyme'
+import React, { Component } from 'react'
+
+export default class CatList extends Component {
+    render() {
+        console.log(this.props.catPics[0])
+        return(
+            <div>{this.props.catPics.map(pic => <img src={pic.url} />)}</div>
+        )
+    }
+    
+}
