932eee1d27bd3c3c05618d7ccb7d3c9112b06ce8
wtwerner <wtwerner@gmail.com>
2021-02-07 11:17:18 -0600

    commit tree 93123c1c845067146d5e42d8cfc1cdd393fb36c9

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,8 @@
-// write and export your action creator function here
+export const fetchCats = () => {
+    return (dispatch) => {
+        dispatch({ type: 'LOADING_CATS' });
+        fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+            .then(response => response.json())
+            .then(cats => dispatch({ type: 'ADD_CATS', cats: cats.images }))
+    };
+}
